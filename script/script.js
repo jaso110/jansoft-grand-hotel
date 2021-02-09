a0cf90e663637b43c284796981c9491c21327cc6
Jan Sørensen <jansorensen@Jans-iMac.mydomain>
2021-02-09 15:29:41 +0100

    init commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+let i = 1;
+
+document.querySelector('.image-1').addEventListener('click', () => {
+    document.querySelector('.image-1').style.zIndex = i
+    i += 1
+})
+
+document.querySelector('.image-2').addEventListener('click', () => {
+    document.querySelector('.image-2').style.zIndex = i
+    i += 1
+})
+
+document.querySelector('.image-3').addEventListener('click', () => {
+    document.querySelector('.image-3').style.zIndex = i
+    i += 1
+})
+
+document.querySelector('.image-4').addEventListener('click', () => {
+    document.querySelector('.image-4').style.zIndex = i
+    i += 1
+})
